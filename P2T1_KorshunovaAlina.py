68572726ce92a991c4fb2aadcfbf3d48576c19d9
korshunovalina <56610351+korshunovalina@users.noreply.github.com>
2020-03-03 10:36:35 -0500

    Add files via upload
    First submission

---
+++ P2T1_KorshunovaAlina.py
@@ -0,0 +1,18 @@
+# This project will calculate projected total sales
+# Date 2/20/2020
+# CTI-110 P2T1 - Sales Prediction
+# Alina Korshunova
+#
+
+
+projectedTotalSales = float(input("Enter the projected amount " + \
+                                  " of total sales: " ))
+profit = 0.23 * projectedTotalSales
+print("The profit is " + format(profit,',.2f') )
+
+
+
+
+
+
+
